0efb4bf20c0d6ae4a7049775132560dda6b7f772
djlim00 <127932430+djlim00@users.noreply.github.com>
2024-07-28 12:10:01 +0900

    Update WebConfig.java

--- rematic-server/src/main/java/com/kuit3/rematicserver/config/WebConfig.java
+++ rematic-server/src/main/java/com/kuit3/rematicserver/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/auth/test")
                 .addPathPatterns("/search/**")
                 .excludePathPatterns("/search/post/guest")
-                //.addPathPatterns("/post/**")
+                .addPathPatterns("/post/**")
                 .excludePathPatterns("/post")
                 .addPathPatterns("/user/**"); // 이게 없으면 인터셉터가 적용이 안되네요...
     }
