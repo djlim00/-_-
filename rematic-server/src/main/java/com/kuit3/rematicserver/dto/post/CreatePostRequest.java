a5a7d73e59fa404747bca68b2e75e272bce1d398
JangIkhwan <bluensky0213@gmail.com>
2024-08-01 20:31:02 +0900

    [Fix] json 요청 객체 수정

--- rematic-server/src/main/java/com/kuit3/rematicserver/dto/post/CreatePostRequest.java
+++ rematic-server/src/main/java/com/kuit3/rematicserver/dto/post/CreatePostRequest.java
@@ -9,13 +9,17 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreatePostRequest {
+    @JsonProperty("title")
     private String title;
+    @JsonProperty("content")
     private String content;
+    @JsonProperty("has_image")
     private Boolean has_image;
     private String category;
     private String genre;
     @JsonProperty("anonymity")
     private Boolean anonymity;
     private Long user_id;
+    @JsonProperty("bulletin_id")
     private Long bulletin_id;
 }
