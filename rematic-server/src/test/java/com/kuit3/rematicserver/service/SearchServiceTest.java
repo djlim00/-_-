358e86d39beda45fd56a0fd0380420ed3309705c
bingbonbon <parkjongikyo@naver.com>
2024-08-08 18:41:31 +0900

    complete code for search

---
+++ rematic-server/src/test/java/com/kuit3/rematicserver/service/SearchServiceTest.java
@@ -0,0 +1,57 @@
+package com.kuit3.rematicserver.service;
+
+import com.kuit3.rematicserver.common.exception.UserKeywordException;
+import com.kuit3.rematicserver.dao.SearchDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.mockito.BDDMockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SearchServiceTest {
+
+    @InjectMocks
+    private SearchService searchService;
+
+    @Mock
+    private SearchDao searchDao;
+
+    @Test
+    @Transactional
+    @DisplayName("최근 검색어 존재시 검색어 삭제")
+    void keywordDeletionTest() {
+        //given
+        Long userId = 0L;
+        Long keywordId = 0L;
+        //키워드가 있는 상황 산정
+        given(searchDao.checkUserRecentKeyword(userId, keywordId)).willReturn(true);
+        given(searchDao.modifyUserRecentKeyword(userId, keywordId)).willReturn(1);
+        //when
+        String consequence = searchService.deactivateUserKeyword(userId, keywordId);
+        //then
+        assertEquals(consequence, "deleted keyword successfully");
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("최근 검색어가 없을 때 검색어 삭제")
+    void KeywordDeletionFailTest() {
+        Long userId = 0L;
+        Long keywordId = 0L;
+        //given
+        given(searchDao.checkUserRecentKeyword(userId, keywordId)).willReturn(false);
+        //then
+        assertThrows(UserKeywordException.class, () -> {
+            searchService.deactivateUserKeyword(userId, keywordId);
+        });
+    }
+
+}
+
