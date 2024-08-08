3861eaf4cf914c015f95e81e5676c234d0a2de8c
bingbonbon <parkjongikyo@naver.com>
2024-08-08 18:04:35 +0900

    complete test for user mypage

--- rematic-server/src/test/java/com/kuit3/rematicserver/service/GetMyPageServiceTest.java
+++ rematic-server/src/test/java/com/kuit3/rematicserver/service/GetMyPageServiceTest.java
@@ -14,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.junit.jupiter.api.Assertions.*;
 
 import static org.mockito.BDDMockito.*;
-import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
