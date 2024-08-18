bfd62e166bd1208b4ebccb22c172b7f1ce670484
JangIkhwan <bluensky0213@gmail.com>
2024-08-18 11:15:29 +0900

    [Fix] 스케줄러 수정

--- rematic-server/src/main/java/com/kuit3/rematicserver/service/RankingSchedulingService.java
+++ rematic-server/src/main/java/com/kuit3/rematicserver/service/RankingSchedulingService.java
@@ -22,8 +22,8 @@ public class RankingSchedulingService {
     private final PostDao postDao;
 
     //정각마다 실행
-//    @Scheduled(cron = "0 0 * * * *")
-    @Scheduled(fixedDelay = 60000)
+    @Scheduled(cron = "0 0 * * * *")
+//    @Scheduled(fixedDelay = 60000)
 
     @Transactional
     public void saveRankingEveryHour() {
